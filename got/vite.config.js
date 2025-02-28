bfa10b119714f5d56509f4564b6085669cd9a135
Kishore Prasath <kishoreprasathece@gmail.com>
2025-02-28 17:09:00 +0530

    done

--- got/vite.config.js
+++ got/vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss()], // This is correct
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['@reduxjs/toolkit'], // Explicitly mark Redux Toolkit as external
+    }
+  }
 });
